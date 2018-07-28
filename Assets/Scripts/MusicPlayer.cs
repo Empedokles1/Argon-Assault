9464ad162930d97adae87cadaa6d0186642a3b85
Gernot Wechselberger <gernot.wechselberger@outlook.com>
2018-07-28 11:44:33 +0200

    Ship Movement

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+    [SerializeField] private AudioClip splashMusic;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {        
+         DontDestroyOnLoad(this.gameObject);       
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = splashMusic;
+        audioSource.Play();
+        Invoke("LoadLevel", 5f);
+    }
+
+    private void LoadLevel()
+    {
+        SceneManager.LoadScene("Level_01");
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float xClampRange = 5f;
+    [SerializeField] float yClampRange = 3f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vertical = CrossPlatformInputManager.GetAxis("Vertical");       
+        float xOffset = horizontal * Time.deltaTime * moveSpeed;
+        float yOffset = vertical * Time.deltaTime * moveSpeed;
+        
+        Vector3 movement = new Vector3(Mathf.Clamp(transform.localPosition.x + xOffset,-xClampRange, xClampRange) , Mathf.Clamp(transform.localPosition.y + yOffset, -yClampRange, yClampRange), transform.localPosition.z);
+        transform.localPosition = movement;
+	}
+}
