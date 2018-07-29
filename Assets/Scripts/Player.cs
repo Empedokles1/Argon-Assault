3be4c3d69bc338977035078a6aed84a2741c6ba3
Gernot Wechselberger <gernot.wechselberger@outlook.com>
2018-07-29 11:57:21 +0200

    Ship Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,46 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-    [SerializeField] float moveSpeed = 10f;
-    [SerializeField] float xClampRange = 5f;
-    [SerializeField] float yClampRange = 3f;
-    // Use this for initialization
+    float horizontal, vertical;
+    float yOffset;
+    [Tooltip("Ship maneuvering speed")] [SerializeField] float moveSpeed = 10f;
+    [Tooltip("Horizontal movement range")] [SerializeField] float xClampRange = 6f;
+    [Tooltip("Vertical movement range")] [SerializeField] float yClampRange = 3f;    
+    [Tooltip("Degrres the ship pitches for every unit moved up/down")] [SerializeField] float positionPitchFactor = -5f;
+    [Tooltip("Degrres the ship yaws for every unit moved left/right")] [SerializeField] float positionYawFactor = 5f;
+    [Tooltip("Nose dipping uo/down while controls pressed")] [SerializeField] float pitchThrow = -25f;    
+    [Tooltip("Banking while controls pressed")] [SerializeField] float bankThrow = -45f;
+    
+    
+
+
     void Start () {
-		
+        yOffset = transform.localRotation.y;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
-        float vertical = CrossPlatformInputManager.GetAxis("Vertical");       
+	
+	void Update ()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+    private void ProcessTranslation()
+    {
+        horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+        vertical = CrossPlatformInputManager.GetAxis("Vertical");
         float xOffset = horizontal * Time.deltaTime * moveSpeed;
         float yOffset = vertical * Time.deltaTime * moveSpeed;
-        
-        Vector3 movement = new Vector3(Mathf.Clamp(transform.localPosition.x + xOffset,-xClampRange, xClampRange) , Mathf.Clamp(transform.localPosition.y + yOffset, -yClampRange, yClampRange), transform.localPosition.z);
+        Vector3 movement = new Vector3(Mathf.Clamp(transform.localPosition.x + xOffset, -xClampRange, xClampRange), Mathf.Clamp(transform.localPosition.y + yOffset, -yClampRange, yClampRange), transform.localPosition.z);
         transform.localPosition = movement;
-	}
+    }
+
+    void ProcessRotation()
+    {
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float pitch = (transform.localPosition.y - yOffset) * positionPitchFactor + vertical * pitchThrow ;
+        float roll = horizontal * bankThrow;
+        transform.localRotation = Quaternion.Euler(pitch,yaw,roll);
+    }
 }
